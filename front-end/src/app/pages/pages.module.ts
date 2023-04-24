631b6b24755da33e1d2ddca398bc6627e15e37ae
yingqing-c <yingqing.c@hotmail.com>
2023-04-25 07:02:46 +0800

    home&footer done

--- front-end/src/app/pages/pages.module.ts
+++ front-end/src/app/pages/pages.module.ts
@@ -11,7 +11,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AddItemComponent,
     ViewItemsComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     CommonModule,
